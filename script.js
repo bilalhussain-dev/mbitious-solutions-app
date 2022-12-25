f0c61605b9c72b0a076469bfaed97fd284711f1a
appsDev <bilalhussian785@gmail.com>
2022-12-25 09:39:45 +0500

    modified app

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-$( '#navbar-active-class' ).on( 'click', function () {
-	$( '#re-menu .nav-menu' ).find( 'li.active' ).removeClass( 'active' );
-	$( this ).parent( 'li' ).addClass( 'active' );
+// Sharing Blog Post
+const shareButton = document.getElementById('share-button');
+
+shareButton.addEventListener('click', () => {
+  navigator.share({
+    title: '10 tips to become a web developer',
+    text: 'Are you interested in becoming a web developer? Check out these 10 tips for improving your skills and getting started in the field.',
+    url: 'https://mbitiousolutions.com/10-tips-to-become-a-web-developer.htm'
+  })
+  .then(() => {
+	alert('Thanks for sharing')
+  })
+  .catch(error => console.log('Error sharing post:', error));
 });
